0bc00fe92b8270612c4afdfe10012be71537e9f4
emxcel <saqib.shaikh@emxcelsolutions.com>
2018-10-04 19:24:39 +0530

    time check

---
+++ app/src/main/java/saqib/com/mvpfullprojectsetup/DummyDate.java
@@ -0,0 +1,26 @@
+package saqib.com.mvpfullprojectsetup;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+
+public class DummyDate {
+
+   /* public static String getPrettyDate(String date) {
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+        try {
+            Date converted = format.parse(date);
+            PrettyTime prettyTime = new PrettyTime(Locale.getDefault());
+            return prettyTime.format(converted);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return "Unknown Time";
+
+    }*/
+    // System.out.println("----** getPrettyDate() : "+getPrettyDate("2018-10-05 14:26:10"));
+    //compile 'org.ocpsoft.prettytime:prettytime:4.0.1.Final'
+}
